b1d0bf7d434ba7b087fdeb4666069f8dd5f7490e
Drsaji <drhealthpass@gmail.com>
2024-11-06 10:33:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scrolling to sections
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
+
+// Popup Contact Form
+function showContactForm() {
+    alert("For more information, please contact us via email at info@sigiriya.com or call +94 123 456 789");
+}
